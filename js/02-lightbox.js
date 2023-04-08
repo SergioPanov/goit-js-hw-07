e9acda97b5520fd8ea48607d7507f1d23dcbcc24
SergioPanov <www.vapanov@gmail.com>
2023-04-08 11:08:38 +0200

    02-gallery-added-js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+const galleryCreate = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryList.innerHTML += galleryCreate;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
 
 console.log(galleryItems);
